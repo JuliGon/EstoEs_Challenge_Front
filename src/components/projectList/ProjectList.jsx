b07fe4a72c7dbbb5a36e30296c62d17348a03db9
juligon <julianagon73@gmail.com>
2023-10-05 14:01:11 -0300

    fixing bugs

--- src/components/projectList/ProjectList.jsx
+++ src/components/projectList/ProjectList.jsx
@@ -28,7 +28,7 @@ export default function ProjectList() {
 	const [projectToDelete, setProjectToDelete] = useState(null);
 
 	const [currentPage, setCurrentPage] = useState(0);
-	const projectsPerPage = 6;
+	const projectsPerPage = 7;
 
 	useEffect(() => {
 		const controller = new AbortController();
